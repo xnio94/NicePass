3cc8680a14a96890e255844acfbfb6e17f9fd180
xnio94 <xnio94@gmail.com>
2021-07-05 19:36:32 +0100

    mvp

--- content.js
+++ content.js
@@ -1,3 +1,70 @@
+//************************************ modal
+function htmlToElement(html) {
+  var template = document.createElement('template');
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+const modalHtml = `<div id="myModal" class="modal center">
+    <div class="modal-content center">
+        <p>` + 'Enter Your Password: anas' + `</p>
+        <input type="password" autocomplete="off" name="mainPassword" id="mainPassword">
+        <br><br>
+        <button class="nicePassButton" id="generatePass">Generate Password</button>
+
+    </div>
+</div>`;
+
+const modalStyle = `<style>
+.modal {
+    display: none; /* Hidden by default */
+    z-index: 1; /* Sit on top */
+    left: 33%;
+    top: 25%;
+    width: 33%; /* Full width */
+    height: auto; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+
+}
+.modal-content {
+    background-color: #b3edad;
+    border:3px solid #12751d;
+    color : #12751d;
+    font-weight: bold;
+    padding: auto;
+}
+
+.center {
+  margin: auto;
+  text-align: center;
+}
+
+.nicePassButton {
+    border-radius:28px;
+    border:1px solid #12751d;
+    cursor:pointer;
+    color:#12751d;
+    font-weight: bold;
+    font-family:Arial;
+    padding: 10px 12px;
+    text-decoration:none;
+    text-align: center;
+}
+.nicePassButton:hover {
+    background-color:#12751d;
+    color: #fff;
+}
+.nicePassButton:active {
+    position:relative;
+    top:1px;
+}
+</style>`;
+
+let nicePassStyle = htmlToElement(modalStyle);
+let nicePassModalElement = htmlToElement(modalHtml);
+
+
 //************************************ using NicePass button
 window.addEventListener('load', function () {
   let inputs = document.getElementsByTagName('input');
@@ -66,7 +133,8 @@ async function generateAndFillPassIn(field) {
   // parse hostname :
   // https://stackoverflow.com/questions/9752963/get-domain-name-without-subdomains-using-javascript
   let host = window.location.hostname;
-  let password = prompt("Enter Your Pin (" + host + ")");
+  //let password = prompt("Enter Your Password (" + host + ")");
+  let password = await myPrompt('Hey');
 
   // var p = new Promise(function (resolve, reject) {
   //   chrome.storage.sync.get("passwordHash", ({passwordHash}) => {
@@ -85,4 +153,36 @@ async function generateAndFillPassIn(field) {
 }
 
 
+function myPrompt(message) {
+  //document.getElementById("myModal")?.remove()
+  document.head.append(nicePassStyle);
+  document.body.append(nicePassModalElement);
+  let passwordField = nicePassModalElement.children[0].children[1];
+  let generate = nicePassModalElement.children[0].children[4];
 
+  passwordField.addEventListener("keyup", function (event) {
+    if (event.code === "Enter") {
+      event.preventDefault();
+      generate.click();
+    }
+  });
+
+  let passwordPromise = new Promise(function (resolve, reject) {
+
+    async function clicked() {
+      resolve(passwordField.value);
+      passwordField.value = "";
+      generate.removeEventListener('click', clicked);
+      nicePassModalElement.remove();//.style.display = "none"
+    }
+
+    generate.addEventListener("click", clicked);
+
+  });
+
+
+  document.getElementById("myModal").style.display = "block";
+  passwordField.focus();
+
+  return passwordPromise;
+}
